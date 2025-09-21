2a48f67a808086dc362915159e7d346c6a91589e
Rikhil Makwana <rikhilmakwana06@gmail.com>
2025-09-21 14:23:57 +0100

    Update Nav to use div instead of ul

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -46,7 +46,7 @@ export default function Navbar() {
     <header className='flex justify-center fixed left-2/4 -translate-x-2/4 z-50 md:w-[504px] w-full'>
         <nav className='relaive flex items-center p-2 border-1 border-[#ffffff14] bg-[#101010] md:rounded-[3rem] backdrop-blur-lg ml-auto mr-auto gap-4 w-full md:justify-start justify-between'>
             <Link href="/" className='text-2xl pl-4 opacity-[.9] font-semibold' onClick={handleNavLinkClick}>Rikhil.</Link>
-            <ul className={`items-center md:flex ${isNavOpen ? 'flex absolute flex-col items-start top-[60px] bg-[#101010] w-full h-screen left-0 right-0' : 'hidden'}`}>
+            <div className={`items-center md:flex ${isNavOpen ? 'flex absolute flex-col items-start top-[60px] bg-[#101010] w-full h-screen left-0 right-0' : 'hidden'}`}>
                 <button onClick={handleCasesClick} className='h-8 px-4 py-2 flex items-center justify-center cursor-pointer md:text-[16px] text-5xl md:my-0 my-10'>
                     Cases
                 </button>
@@ -64,7 +64,7 @@ export default function Navbar() {
                         </g>
                     </svg>
                 </Link>
-            </ul>
+            </div>
             <div className='md:block flex items-center gap-6 md:pr-0 pr-4'>
               <Link href="mailto:rikhilmakwana06@gmail.com?subject=Hello%20Rikhil!" className='btn flex items-center justify-center border-2  border-[#007aff] bg-[#007aff] text-[#f8f8f8] rounded-[3rem] px-4 py-2'>Contact</Link>
               <div className={`hambuger md:hidden cursor-pointer ${isNavOpen && 'active'}`} onClick={() => setIsNavOpen((prev) => !prev)}>
