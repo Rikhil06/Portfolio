b18d7a9dbc7327da2efdb9013f08561137c41c38
Rikhil Makwana <rikhilmakwana06@gmail.com>
2025-09-21 16:42:11 +0100

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config, { isServer }) {
-    // Enable source maps for production builds
-    if (!isServer) {
-      config.devtool = 'source-map'; // You can use other types like 'cheap-module-source-map', 'eval-source-map', etc.
-    }
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
